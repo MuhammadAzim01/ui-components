e1d1be6d6c453b16b6c6842db2244d09e453ba36
ddroid <contactahmadmunir@gmail.com>
2025-11-27 01:35:22 +0500

    Updated Readme and Linted

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ui-components
 see https://playproject.io/ui-components/
+
+
+This is a showcase place for the Theme Widget app components. 
+This app is a simple app that allows you to change the theme of a website and see the changes in real time.
+
+To clone and install, run the following commands:
+
+```bash
+git clone https://github.com/ddroid/ui-components
+cd ui-components
+npm install
+npm run start
+```
--- guide/tabs_commented.js
+++ guide/tabs_commented.js
@@ -1,15 +1,18 @@
-//state Initialization
+// state Initialization
 const STATE = require('STATE')
 const statedb = STATE(__filename)
+// eslint-disable-next-line no-unused-vars
 const { sdb, get } = statedb(fallback_module)
 // exporting the module
 module.exports = component
 // actual module
-async function component(opts, callback = id => console.log('calling:', '@' + id)) {
+async function component (opts, callback = id => console.log('calling:', '@' + id)) {
   // getting the state database for the current instance
+  // eslint-disable-next-line no-unused-vars
   const { id, sdb } = await get(opts.sid)
   // optional getting drive from state database but it does not work currently. will be useful in the future though.
-  const {drive} = sdb
+  // eslint-disable-next-line no-unused-vars
+  const { drive } = sdb
   // on object which contains the functions to be executed when the dataset changes and onbatch is called.
   const on = {
     variables: onvariables,
@@ -21,15 +24,16 @@ async function component(opts, callback = id => console.log('calling:', '@' + id
   const div = document.createElement('div')
   const shadow = div.attachShadow({ mode: 'closed' })
   // defining the HTML structure of the component using template literals.
-  shadow.innerHTML = `<div class="tab-entries"></div>`
+  shadow.innerHTML = '<div class="tab-entries"></div>'
   const entries = shadow.querySelector('.tab-entries')
   // Initializing the variables to be used in the element creation. We store the data from drive through the onbatch function in these variables.
   // this init variable is used to check if the component is initialized or not. It is set to true when the component is initialized for the first time. So that after that we can just update the component instead of creating it again using the onbatch function data.
   let init = false
   let variables = []
   let dricons = []
-  
+
   // subs for storing the Sid's of submodules and onbatch function which is called when the dataset changes.
+  // eslint-disable-next-line no-unused-vars
   const subs = await sdb.watch(onbatch)
   // this is just a custom scrolling through drag clicking functionality.
   if (entries) {
@@ -86,7 +90,7 @@ async function component(opts, callback = id => console.log('calling:', '@' + id
   return div
   // All the functions are defined below this return statement.
   // this create_btn function is executed using forEach on the variables array. It creates the buttons for each variable in the array. It uses the data from the variables and dricons arrays to create the buttons.
-  async function create_btn({ name, id }, index) {
+  async function create_btn ({ name, id }, index) {
     const el = document.createElement('div')
     el.innerHTML = `
     <span class="icon">${dricons[index + 1]}</span>
@@ -101,7 +105,6 @@ async function component(opts, callback = id => console.log('calling:', '@' + id
     label_el.draggable = false
     icon_el.onclick = callback
     entries.appendChild(el)
-    return
   }
   // this function is called when the dataset changes. It calls the functions defined in `on` object.
   function onbatch (batch) {
@@ -119,26 +122,26 @@ async function component(opts, callback = id => console.log('calling:', '@' + id
   // this function throws an error if the type of data is not valid. It is used to handle the errors in the onbatch function.
   function fail (data, type) { throw new Error('invalid message', { cause: { data, type } }) }
   // this function adds styles to shadow DOM. It uses the CSSStyleSheet API to create a new stylesheet and then replaces the existing stylesheet with the new one.
-  function inject_style(data) {
+  function inject_style (data) {
     const sheet = new CSSStyleSheet()
     sheet.replaceSync(data)
     shadow.adoptedStyleSheets = [sheet]
   }
   // we simple store the data from the dataset into variables. We can use this data to create the buttons in the create_btn function.
-  function onvariables(data) {
+  function onvariables (data) {
     const vars = typeof data[0] === 'string' ? JSON.parse(data[0]) : data[0]
     variables = vars
   }
   // same here we store the data into dricons for later use. We can use this data to create the buttons in the create_btn function.
-  function iconject(data) {
+  function iconject (data) {
     dricons = data
   }
   // waiting for the next STATE module to be released so we can use the drive.put() to update the scroll position.
-  function update_scroll_position() {
+  function update_scroll_position () {
     // TODO
   }
 
-  function onscroll(data) {
+  function onscroll (data) {
     setTimeout(() => {
       if (entries) {
         entries.scrollLeft = data
@@ -147,32 +150,32 @@ async function component(opts, callback = id => console.log('calling:', '@' + id
   }
 }
 // this is the fallback module which is used to create the state database and to provide the default data for the component.
-function fallback_module() {
+function fallback_module () {
   return {
-    api: fallback_instance,
+    api: fallback_instance
   }
   // this is the fallback instance which is used to provide the default data for the instances of a component. this also help in providing an API for csustomization by overriding the default data.
-  function fallback_instance() {
+  function fallback_instance () {
     return {
       drive: {
         'icons/': {
-          'cross.svg':{
-            '$ref': 'cross.svg'
-             // data is stored through '$ref' functionality
+          'cross.svg': {
+            $ref: 'cross.svg'
+            // data is stored through '$ref' functionality
           },
           '1.svg': {
-            '$ref': 'icon.svg'
+            $ref: 'icon.svg'
           },
           '2.svg': {
-            '$ref': 'icon.svg'
+            $ref: 'icon.svg'
           },
           '3.svg': {
-            '$ref': 'icon.svg'
+            $ref: 'icon.svg'
           }
         },
         'variables/': {
           'tabs.json': {
-            '$ref': 'tabs.json'
+            $ref: 'tabs.json'
           }
         },
         'scroll/': {
@@ -182,7 +185,7 @@ function fallback_module() {
         },
         'style/': {
           'theme.css': {
-            '$ref': 'style.css'
+            $ref: 'style.css'
           }
         }
       }
